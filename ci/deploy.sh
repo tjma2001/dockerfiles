b7c195eee002feed241c05b2b25bd44410ea28a4
Taariq Mullins <tjma2001@gmail.com>
2018-04-08 01:37:00 +0200

    refactor(ci): pushing latest

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,3 +2,4 @@
 VERSION=$(cat rust/VERSION)
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push tensaibankai/rust:${VERSION}
+docker push tensaibankai/rust:latest
