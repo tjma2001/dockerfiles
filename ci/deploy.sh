ada3c87926191938b43d3068b868c7aeccb06679
Taariq Mullins <tjma2001@gmail.com>
2018-04-12 03:06:36 +0200

    refactor(deploy): updated version

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-VERSION=$(cat rust/VERSION)
+
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+VERSION=$(cat rust/VERSION)
 docker push tensaibankai/rust:${VERSION}
 docker push tensaibankai/rust:latest
+
+VERSION=$(cat node/VERSION)
 docker push tensaibankai/node:${VERSION}
 docker push tensaibankai/node:latest
